4408cde68d892fd23595e66fd41ceeacf2ed0a8f
Von C <shervonc@gmail.com>
2019-07-18 20:17:04 +0000

    Automatically backed up by Learn

--- objects.js
+++
@@ -1,7 +0,0 @@
-function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
-  obj[key] = value
-  return obj
-}
-
-const recipe = { eggs:3}
-destructivelyUpdateObjectWithKeyAndValue(recipe, "flour", "3 cups")
