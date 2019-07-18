af70f178018acc03e24805818b2b60d2098cf1a8
Von C <shervonc@gmail.com>
2019-07-18 20:26:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Phil Ochs: "Don't Give Up"}
+var playlist = {Phil Ochs: "My Bloody Valentine"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {[artistName]:"songTitle"})
